171b136b0f6e52115179a602aed5dbf3a402b7e0
mlh <mlh@contraste.com>
2024-10-28 13:54:15 +0100

    Adding wizards

---
+++ hospital/wizards/add_appointment.py
@@ -0,0 +1,20 @@
+from odoo import models, fields, api
+
+
+class AddAppointments(models.TransientModel):
+    _name = 'add.appointment'
+
+    name = fields.Char(compute='_compute_header_name')
+    patient_id = fields.Many2one('res.partner', string='Patient', required=True, domain="[('is_patient', '=', True)]")
+    notes = fields.Text(string='Notes')
+    app_date = fields.Datetime(string='Date Time', required=True)
+    doctor_id = fields.Many2one('res.users', 'Doctor', domain="[('is_doctor', '=', True)]")
+
+    @api.depends('patient_id')
+    def _compute_name(self):
+        print('Trigger from add appointment')
+        for record in self:
+            record.name = f"Adding an appointment for {record.patient_id.name}"
+
+    def confirm_appointment(self):
+        print('ok')
